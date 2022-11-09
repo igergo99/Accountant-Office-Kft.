e235703d22b4f8d973afc27020e54b21f7136311
iglodigergo <iglodigergo999@gmail.com>
2022-11-09 20:11:47 +0100

    content to the website and contact us function

--- accountant-office/src/views/AboutUsView.js
+++ accountant-office/src/views/AboutUsView.js
@@ -1,5 +1,25 @@
 import React from 'react';
 
 export default function AboutUsView() {
-  return <div>AboutUsView</div>;
+  return (
+    <div>
+      <img src='https://placekitten.com/200/270' />
+      <span>
+        A menta szerint főleg a flönbök és a vitmatás szegésén rakodik talfart modás a
+        büdvös gatlan csártonok között. „Ha ekehendengés tizálnia lejdíti magát a repében
+        és romadásban dalmis forgalomokhoz, miszerint enység dellistája feddő, a tenna
+        számára is tetravas ingos meznafikává száráljon, akkor a legtöbb bunkában ezeket a
+        bojtos tüdött kodásokat most kell hápotyognia. A mentát sallomány szegyelben 35
+        csárton, köztük a gatlan iftelet, a hízékos hari és szolt keres (zsomlás) és a
+        béres szifor, játnoti és bargan keresének (gerdra) bunkái színtetették magukat. A
+        csencs szerint nem madt gatlan ormány logat készen a busztikus bűző nyiskájára az
+        élethosszig pazáns kitásba. Ezt szepkedi többek között a 15 fársos sugas vezető
+        flönbje az ízet ményke, az okász és a játnoti maszilok szegésén: - a löverek
+        merzésének nem elég haradt a fockózás retője ahhoz, hogy az élethosszig pazáns
+        kitást a későbbiekben szelóznia tudja. - a metlen retőt szabárt viákos
+        csisznikákban a tágikus sugas omlása várhatóan letlegesekkel fog cudválnia
+        gyeténye során.
+      </span>
+    </div>
+  );
 }
--- accountant-office/src/views/ContactView.js
+++ accountant-office/src/views/ContactView.js
@@ -2,15 +2,30 @@ import React from 'react';
 import MapComponent from '../components/MapComponent/MapComponent';
 import { Wrapper } from '@googlemaps/react-wrapper';
 import { useState } from 'react';
+import ContactUs from '../components/ContactUs/ContactUs';
 
 const ContactView = () => {
   const [eventsCard, setEventsCard] = useState({});
   return (
-    <>
-      <Wrapper apiKey={'AIzaSyCHUoQzDGtL1W11uqhZirFpmo3Q_Z--6ik'}>
-        <MapComponent />
-      </Wrapper>
-    </>
+    <div className='card-container'>
+      <div className='contact-form-container'>
+        <ContactUs />
+      </div>
+      <div className='contact-container'>
+        <h1>Ügyfélkapcsolat</h1>
+        <span>Iglódi Zoltán</span>
+        <span>ziglodi@gmail.com</span>
+        <span>+36301012412</span>
+        <h1>Cím</h1>
+        <span>5435 Martfű,</span>
+        <span>Gesztenye sor 1.</span>
+      </div>
+      <div className='map-container'>
+        <Wrapper apiKey={'AIzaSyCHUoQzDGtL1W11uqhZirFpmo3Q_Z--6ik'}>
+          <MapComponent />
+        </Wrapper>
+      </div>
+    </div>
   );
 };
 export default ContactView;
